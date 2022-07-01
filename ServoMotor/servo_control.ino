861b8bf468398523c90cd8d1e461e4eec92c268c
astianmuchui <sebastianmuchui79@gmail.com>
2022-07-01 15:28:34 +0300

    Control two servo motors simultaneously

---
+++ ServoMotor/servo_control.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+// Instantiate object
+Servo servo; 
+Servo servo_two;
+int pos = 3;    
+int pos_two = 10;
+
+
+void setup() {
+  // Almost the same as pinMode function
+  servo.attach(3);  
+  servo_two.attach(10);
+}
+
+void loop() {
+  
+  // Read potentiometer value 
+  int val = analogRead(A0);
+
+  // Map values since the motor might need 
+  int mapped = map(val,0,1023,0,255);
+
+  servo.write(mapped);
+  servo_two.write(mapped);
+}
+
